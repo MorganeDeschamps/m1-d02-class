4700211c390d1283a1303e65cf52cfcbc58cedae
Marco Santonastasi <marco.santonastasi@gmail.com>
2021-06-01 17:09:49 +0200

    WIP step 3 retur lazy

--- 02_code-js_functions_intro.js
+++ 02_code-js_functions_intro.js
@@ -1,7 +1,17 @@
 /**
  * Code along lesson material for JS Funtions
  */
+
 const peopleILike = ["Marco", "Fede", "Sebas", "Fran"];
+
+function lazyFunction() {
+  const iAmLazy = "Yes";
+  // Do I return anything????
+  console.log("What are you doing!?!?!?");
+  return iAmLazy; // return undefined because console.log() return itself undefined
+}
+
+console.log("lazyFunction", lazyFunction());
 
 function sayHola(person) {
   //We don't have a value yet
