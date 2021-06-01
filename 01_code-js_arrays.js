14d2257fc0fca9cda92b595811de67a664c098f2
Marco Santonastasi <marco.santonastasi@gmail.com>
2021-06-01 15:45:48 +0200

    WIP step 2

--- 01_code-js_arrays.js
+++ 01_code-js_arrays.js
@@ -4,10 +4,27 @@
 
 const aNewArray = ["Morgan", "Raul", "Miki", 2, false, NaN];
 
+const concatArray = aNewArray.concat([1, 2, 7]);
+console.log(concatArray.length);
+console.log(concatArray);
+
 for (element of aNewArray) {
   //let element = aNewArray[i];
   console.log(element);
 }
+
+console.log(aNewArray);
+
+aNewArray.push("cappucino");
+aNewArray.unshift("caffé");
+
+console.log(aNewArray.length);
+console.log(aNewArray);
+
+aNewArray.pop();
+aNewArray.shift();
+console.log(aNewArray.length);
+console.log(aNewArray);
 
 /*
 for (let i = 0; i < aNewArray.length; i++) {
