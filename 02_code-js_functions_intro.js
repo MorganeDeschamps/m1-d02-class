f6ed8dc3101a12593bf6b1a90d47e3825f622347
Marco Santonastasi <marco.santonastasi@gmail.com>
2021-06-01 16:31:47 +0200

    WIP step 1

--- 02_code-js_functions_intro.js
+++ 02_code-js_functions_intro.js
@@ -1,3 +1,18 @@
 /**
  * Code along lesson material for JS Funtions
  */
+function banana(person1 = 1, person2 = 2) {
+  return `${person1} and ${person2} look like a banana`;
+}
+
+function sayHola() {
+  return "Hola Mika!";
+}
+
+function capuccino() {
+  return "Ciao!";
+}
+
+const returnValue = banana();
+
+console.log(returnValue);
