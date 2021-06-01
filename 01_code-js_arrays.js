5000dda52a3bdc4c9a954be75b1a79004fc60e6d
Marco Santonastasi <marco.santonastasi@gmail.com>
2021-06-01 14:46:44 +0200

    WIP step 1

--- 01_code-js_arrays.js
+++ 01_code-js_arrays.js
@@ -1,3 +1,17 @@
 /**
  * Code along lesson material for JS Arrays
  */
+
+const aNewArray = ["Morgan", "Raul", "Miki", 2, false, NaN];
+
+for (element of aNewArray) {
+  //let element = aNewArray[i];
+  console.log(element);
+}
+
+/*
+for (let i = 0; i < aNewArray.length; i++) {
+  let element = aNewArray[i];
+  console.log(element);
+}
+*/
